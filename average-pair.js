a56d76e412d49b9fce54d7b3e3f052ef06332044
Jerry Hsu <Jerry>
2021-10-27 22:34:57 -0400

    constructNote & averagePair

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,19 @@
-// add whatever parameters you deem necessary
-function averagePair() {
+// Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average.
+
+function averagePair(arr,avg) {
+  let [left, right] = [0, arr.length-1];
+  let pairAvg;
+  while (left < right) {
+    pairAvg = (arr[left] + arr[right])/2;
+    if (pairAvg === avg) {
+      return true;
+    } else if (pairAvg < avg) {
+      left ++;
+    } else {
+      right--;
+    }
+  }
+  return false;
 }
+
+module.exports = { averagePair };
--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,24 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(msg,letters) {
+  const msgArr = letterCount(msg);
+  const ltrArr = letterCount(letters);
+
+  for (let k of msgArr.keys()) {
+    if (!ltrArr.get(k)) return false;
+    if (msgArr.get(k) > ltrArr.get(k)) return false;
+  }
+  return true;
 }
+
+function letterCount(str){
+  const letters = str.split("");
+  let frequencies = new Map();
+
+  for (let el of letters) {
+    let count = frequencies.get(el) || 0;
+    frequencies.set(el, count+1);
+  }
+  return frequencies;
+}
+
+module.exports = { constructNote };
