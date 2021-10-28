00d8f6357d755f377d7e88f3750b7ba81be77f1c
Jerry Hsu <Jerry>
2021-10-28 15:20:48 -0400

    longestFall

--- longest-fall.js
+++ longest-fall.js
@@ -1,3 +1,18 @@
-// add whatever parameters you deem necessary
-function longestFall() {
+// accepts an array of integers, and returns the length of the longest consecutive decrease of integers
+function longestFall(arr) {
+  let [idxLast, idxCurr] = [0,1];
+  let longest = 1;
+  if (arr.length<=1) return arr.length;
+  while (idxCurr < arr.length){
+    if (arr[idxCurr] < arr[idxCurr-1]) {
+      let diff = idxCurr - idxLast + 1;
+      longest = diff > longest ? diff : longest;
+    } else {
+      idxLast = idxCurr;
+    }
+    idxCurr++;
+  }
+  return longest;
 }
+
+module.exports = { longestFall };
